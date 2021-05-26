9f3c6e45f782b9c43473b957386901c2e8432ec7
Christopher II Lajom <penncorleone@gmail.com>
2021-05-26 10:53:21 +0800

    Created Lorem Ipsum Generator Mobile Responsive.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import text from "./data";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState("");
+  const [content, setContent] = useState([]);
+
+  useEffect(() => {}, []);
+
+  const handleChange = (e) => {
+    setCount(e.target.value);
+  };
+
+  const handleGenerate = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count < 0) {
+      amount = 0;
+    }
+    if (count > text.length) {
+      amount = text.length - 1;
+    }
+
+    setContent(text.slice(0, amount));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="heading">
+        <h1>Lorem Ipsum Generator Paragraph</h1>
+      </div>
+      <div className="form">
+        <label htmlFor="paragraph">Paragraphs</label>
+        <input
+          type="number"
+          name="paragraph"
+          value={count}
+          onChange={handleChange}
+        />
+        <button onClick={handleGenerate}>Generate</button>
+      </div>
+      {content.map((c, index) => {
+        return (
+          <div className="content-container">
+            <p className="content">{c}</p>
+          </div>
+        );
+      })}
+    </>
   );
-}
+};
 
 export default App;
